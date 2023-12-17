2f8305335c49bca0e9282270a933ee79022abbed
Yi Zhang <zhangyi@microsoft.com>
2023-12-17 15:24:16 -0800

    args

--- evaluation/js/src/util/fetchBingTile.ts
+++ evaluation/js/src/util/fetchBingTile.ts
@@ -1,5 +1,5 @@
-console.log("Fetch bing tile util");
 import * as MapHelper from "./MapHelper";
+import path from "path";
 
 const locationDictionary = {
     seattle: {
@@ -9,7 +9,19 @@ const locationDictionary = {
     newyork: {
         lat: 40.71589,
         lon: -74.065106,
-    }
+    },
+    berlin: {
+        lat: 52.506126,
+        lon: 13.353309,
+    },
+    london: {
+        lat: 51.500212,
+        lon: -1.218655,
+    },
+    nowhere: {
+        lat: 19.931847,
+        lon: -177.326319,
+    },
 };
 
 const buildArgsDictionary = (args) => {
@@ -25,13 +37,35 @@ const buildArgsDictionary = (args) => {
     return argsDictioanry;
 };
 
-console.log(buildArgsDictionary(process.argv));
+const currentPath = path.parse(process.argv[1]);
+const bingTilePath = path.resolve(currentPath.dir, "../../../../test/fixtures/bing/mvt");
 
-const resultTileSet = MapHelper.getViewTileSets(
-    locationDictionary["newyork"].lat,
-    locationDictionary["newyork"].lon,
-    8,
-    1024,
-    768
-);
-console.log(resultTileSet);
+if (process.argv.length < 4) {
+    console.log(process.argv);
+    console.log("Fetch bing tile util example");
+    console.log("npx ts-node src/util/fetchBingTile.ts -center newyork");
+} else {
+    const argsDict = buildArgsDictionary(process.argv);
+    const centerName = (argsDict["-center"] as string).toLowerCase();
+    const center = locationDictionary[centerName];
+
+    const mockScreenWidth = 1024;
+    const mockScreenHeight = 768;
+
+    if (!center) {
+        console.log("Center value must be one of these valuse: ");
+        console.log(Object.keys(locationDictionary));
+    } else {
+        for (let z = 2; z < 20; z++) {
+            const resultTileSet = MapHelper.getViewTileSets(
+                center.lat,
+                center.lon,
+                z,
+                mockScreenWidth,
+                mockScreenHeight,
+            );
+
+            console.log(resultTileSet);
+        }
+    }
+}
