f6eec88665c02bcd84b952a98ce2bd23a4f265fd
springmeyer <dane.springmeyer@gmail.com>
2024-05-24 12:26:35 -0700

    attempt to make the jni_md.h header cross-platform

--- encodings/fsst/java/fsst-wrapper/resources/jni_md.h
+++ encodings/fsst/java/fsst-wrapper/resources/jni_md.h
@@ -26,15 +26,28 @@
 #ifndef _JAVASOFT_JNI_MD_H_
 #define _JAVASOFT_JNI_MD_H_
 
-#ifndef JNIEXPORT
-  #define JNIEXPORT __declspec(dllexport)
+#ifdef _WIN32
+  #ifndef JNIEXPORT
+    #define JNIEXPORT __declspec(dllexport)
+  #endif
+  #define JNIIMPORT __declspec(dllimport)
+  #define JNICALL __stdcall
+  // 'long' is always 32 bit on windows so this matches what jdk expects
+  typedef long jint;
+  typedef __int64 jlong;
+  typedef signed char jbyte;
+#else
+  #define JNIEXPORT __attribute__ ((visibility ("default")))
+  #define JNIIMPORT
+  #define JNICALL
+  #if defined(__LP64__) && __LP64__ /* for -Wundef */
+  typedef int jint;
+  #else
+  typedef long jint;
+  #endif
+  typedef long long jlong;
+  typedef signed char jbyte;
 #endif
-#define JNIIMPORT __declspec(dllimport)
-#define JNICALL __stdcall
 
-// 'long' is always 32 bit on windows so this matches what jdk expects
-typedef long jint;
-typedef __int64 jlong;
-typedef signed char jbyte;
 
 #endif /* !_JAVASOFT_JNI_MD_H_ */
