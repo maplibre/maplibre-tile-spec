e22b0335b2d5ae988a809d35e9eae1b5c36ca54e
springmeyer <dane.springmeyer@gmail.com>
2024-05-28 20:33:27 -0700

    remove compile.bat, attempt to fix windows include paths

--- converter/java/resources/compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-g++ -std=c++11 -shared -I. -I..\resources -L. -lfsst FsstWrapper.cpp -o FsstWrapper.dll -lstdc++ -Wl,-rpath
-IF %ERRORLEVEL% EQU 0 (
-    copy fsst.dll ..\.
-    copy FsstWrapper.dll ..\.
-)
