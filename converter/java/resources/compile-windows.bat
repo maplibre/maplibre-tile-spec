d3704e14db391b5e1b46539a000e612c12427c69
springmeyer <dane.springmeyer@gmail.com>
2024-05-31 08:24:10 -0700

    switch to cmake

--- converter/java/resources/compile-windows.bat
+++ converter/java/resources/compile-windows.bat
@@ -1,7 +1,15 @@
 @echo off
-echo "Compiling on windows"
-echo %cd%
-bash resources/compile
+echo %cd%IF EXIST build\FsstWrapper.so (
+    echo "FsstWrapper.so exists, skipping compilation"
+    echo "  Remove ./build/FsstWrapper.so to recompile"
+    echo "  Remove ./build to reconfigure & compiled"
+) ELSE (
+    echo "FsstWrapper.so does not exist, building now"
+    mkdir -p build
+    cd build
+    cmake ../Resources
+    cmake --build . --config Release
+)
 IF %ERRORLEVEL% EQU 0 (
     echo "Compilation successful"
     exit /b 0
