4147b6e2bf72af30d71ebe991b94a8de228e15f9
springmeyer <dane.springmeyer@gmail.com>
2024-06-03 17:07:16 -0700

    update fixtures path

--- java/src/test/java/com/mlt/TestConstants.java
+++ java/src/test/java/com/mlt/TestConstants.java
@@ -3,7 +3,7 @@ package com.mlt.test.constants;
 import java.nio.file.Paths;
 
 public class TestConstants {
-    public static final String OMT_MVT_PATH = Paths.get("..","..","test","fixtures","omt").toString();
-    public static final String BING_MVT_PATH = Paths.get("..","..","test","fixtures","bing").toString();
-    public static final String AMZ_HERE_MVT_PATH = Paths.get("..","..","test","fixtures","amazon_here").toString();
+    public static final String OMT_MVT_PATH = Paths.get("..","test","fixtures","omt").toString();
+    public static final String BING_MVT_PATH = Paths.get("..","test","fixtures","bing").toString();
+    public static final String AMZ_HERE_MVT_PATH = Paths.get("..","test","fixtures","amazon_here").toString();
 }
