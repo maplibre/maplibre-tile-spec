4d9b5b3efda2d9c6cbd082b807179d1009fe57dc
Dane Springmeyer <dane.springmeyer@gmail.com>
2024-06-07 07:27:10 -0700

    minor cleanup (#90)

--- java/src/main/java/com/mlt/converter/encodings/StringEncoder.java
+++ java/src/main/java/com/mlt/converter/encodings/StringEncoder.java
@@ -10,7 +10,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StringEncoder {
