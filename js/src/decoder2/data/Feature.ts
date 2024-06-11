8f7ed4b7b7eaff63f63df61f8fc3f11b0d76cd13
springmeyer <dane.springmeyer@gmail.com>
2024-06-10 23:35:41 -0700

    minor cleanups

--- js/src/decoder2/data/Feature.ts
+++ js/src/decoder2/data/Feature.ts
@@ -1,10 +1,8 @@
-// import { Geometry } from 'geojson';
-
 export class Feature {
     id: number;
     geometry: string;
     properties: { [key: string]: any };
-
+    // TODO: Add geometry type
     constructor(id: number, geometry: string, properties: { [key: string]: any }) {
         this.id = id;
         this.geometry = geometry;
