71008889c781c6ddd975f48c6cdef2589882c970
springmeyer <dane.springmeyer@gmail.com>
2024-06-11 00:01:59 -0700

    leave geometry untyped for now

--- js/src/decoder2/data/Feature.ts
+++ js/src/decoder2/data/Feature.ts
@@ -3,7 +3,7 @@ export class Feature {
     geometry: string;
     properties: { [key: string]: any };
     // TODO: Add geometry type
-    constructor(id: number, geometry: string, properties: { [key: string]: any }) {
+    constructor(id: number, geometry, properties: { [key: string]: any }) {
         this.id = id;
         this.geometry = geometry;
         this.properties = properties;
