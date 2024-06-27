723ae6f1a5c60a412f7989c62b51611c50302dc0
Eric Brelsford <ebrelsford@gmail.com>
2024-06-27 13:30:53 -0400

    Remove mlt-vector-tile-js import/export in main entrypoint

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,4 +1,2 @@
-import * as MltVectorTileJs from './mlt-vector-tile-js/index';
-export { MltVectorTileJs };
 export { MltDecoder } from './decoder/MltDecoder';
 export { TileSetMetadata } from './metadata/mlt_tileset_metadata_pb';
