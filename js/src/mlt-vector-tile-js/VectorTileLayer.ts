927654cc5c87fbd4b47922fbbbb0ab33d4e69e2b
Eric Brelsford <ebrelsford@gmail.com>
2024-06-27 13:37:19 -0400

    Load features lazily

--- js/src/mlt-vector-tile-js/VectorTileLayer.ts
+++ js/src/mlt-vector-tile-js/VectorTileLayer.ts
@@ -7,19 +7,16 @@ class VectorTileLayer {
   extent: number;
   length: number = 0;
 
-  _raw: any;
-  _keys: string[];
-  _values: any[];
-  _features: VectorTileFeature[] = [];
+  _raw: Layer;
 
   constructor(layer: Layer) {
     this.name = layer.name;
-    this._features = layer.features.map((feature) => new VectorTileFeature(feature));
-    this.length = this._features.length;
+    this._raw = layer;
+    this.length = layer.features.length;
   }
 
   feature(i: number): VectorTileFeature {
-    return this._features[i];
+    return new VectorTileFeature(this._raw.features[i]);
   }
 }
 
