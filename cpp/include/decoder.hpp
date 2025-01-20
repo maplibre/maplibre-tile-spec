60fb80df9e033abc0abe1d0ac6493c8da3abae47
Tim Sylvester <tim@wetdogweather.com>
2025-01-20 15:04:01 -0800

    add fastpfor

--- cpp/include/decoder.hpp
+++ cpp/include/decoder.hpp
@@ -5,13 +5,22 @@
 #include <metadata/tileset.hpp>
 #include <tile.hpp>
 
+#include <memory>
+
 namespace mlt::decoder {
 
 class Decoder {
 public:
     using TileSetMetadata = metadata::tileset::TileSetMetadata;
 
+    Decoder();
+    ~Decoder();
+
     MapLibreTile decode(DataView, const TileSetMetadata&);
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl;
 };
 
 } // namespace mlt::decoder
