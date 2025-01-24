3b05111777e95d77b503c8644b227a218549822b
Thomas Böhm <thomas-a.boehm@rohde-schwarz.com>
2025-01-24 07:44:07 +0100

    Benchmark use only fastpfor, without extra varint block

--- js/bench/fastpfor.ts
+++ js/bench/fastpfor.ts
@@ -1,7 +1,7 @@
 import Benchmark from "benchmark";
 import fs from "fs";
-import {FastPFORDecoder} from "../src/encodings/fastpfor";
 import {VarInt} from "../src/encodings/fastpfor/varint";
+import {FastPFOR} from "../src/encodings/fastpfor/fastpfor";
 
 
 const suite = new Benchmark.Suite;
@@ -16,23 +16,27 @@ const suite = new Benchmark.Suite;
 //     }
 // }
 
-const VARINT_ENCODED_VALUES = new Uint32Array(fs.readFileSync("../test/data/250k_ascending_varint.bin").buffer);
-const FASTPFOR_ENCODED_VALUES = new Uint32Array(fs.readFileSync("../test/data/250k_ascending_fastpfor.bin").buffer);
+const VARINT_ENCODED_VALUES = new Uint32Array(fs.readFileSync("test/data/250k_ascending_varint.bin").buffer);
+const FASTPFOR_ENCODED_VALUES = new Uint32Array(fs.readFileSync("test/data/250k_ascending_fastpfor.bin").buffer);
 
-const fastPfor = FastPFORDecoder.default();
+const fastPfor = FastPFOR.default();
 const varint = VarInt.default();
-const output: Uint32Array = new Uint32Array(250_000);
+const fastpfor_output: Uint32Array = new Uint32Array(250_000);
+const varint_output: Uint32Array = new Uint32Array(250_000);
 
 suite.add("FastPFOR decoding", function () {
     fastPfor.uncompress({
         input: FASTPFOR_ENCODED_VALUES,
-        output: output,
+        inpos: 0,
+        inlength: FASTPFOR_ENCODED_VALUES.length,
+        output: fastpfor_output,
+        outpos: 0,
     });
 })
     .add("VarInt decoding", function () {
         varint.uncompress({
             input: VARINT_ENCODED_VALUES,
-            output: output,
+            output: varint_output,
         });
     })
     .on('cycle', (event: Benchmark.Event) => {
