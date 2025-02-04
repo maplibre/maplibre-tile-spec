e490d568d8590307edfe8577fecd8b968c7fd2ec
Tim Sylvester <tim@wetdogweather.com>
2025-02-04 13:20:12 -0800

    Fix vectorized delta decoding, add test cases

--- cpp/src/mlt/util/vectorized.hpp
+++ cpp/src/mlt/util/vectorized.hpp
@@ -7,24 +7,39 @@
 #include <vector>
 
 namespace mlt::util::decoding::vectorized {
-
 namespace {
+/// JS/Java `>>>`
 template <int bits, typename T>
+    requires(std::integral<T>)
 T unsigned_rshift(T t) {
     return static_cast<T>(static_cast<std::make_unsigned_t<T>>(t) >> bits);
 }
+
+/// Shifts the ones bit into the sign, for reasons
+template <typename T>
+    requires(std::integral<T>)
+T odd_sign(T t) {
+    return (t << 31) >> 31;
 }
+
+template <typename T>
+    requires(std::integral<T>)
+T shift_and_xor(T t) {
+    return unsigned_rshift<1>(t) ^ odd_sign(t);
+}
+
+} // namespace
 
 template <typename T>
     requires(std::is_integral_v<T> && sizeof(T) == 4)
 inline void decodeComponentwiseDeltaVec2(std::vector<T>& data) noexcept {
     assert((data.size() % 2) == 0);
     if (1 < data.size()) {
-        data[0] = unsigned_rshift<1>(data[0]) ^ ((data[0] << 31) >> 31);
-        data[1] = unsigned_rshift<1>(data[1]) ^ ((data[1] << 31) >> 31);
+        data[0] = shift_and_xor(data[0]);
+        data[1] = shift_and_xor(data[1]);
         for (std::size_t i = 2; i + 1 < data.size(); i += 2) {
-            data[i] = unsigned_rshift<1>(data[i]) ^ ((data[i] << 31) >> 31) + data[i - 2];
-            data[i + 1] = unsigned_rshift<1>(data[i + 1]) ^ ((data[i + 1] << 31) >> 31) + data[i - 1];
+            data[i] = shift_and_xor(data[i]) + data[i - 2];
+            data[i + 1] = shift_and_xor(data[i + 1]) + data[i - 1];
         }
     }
 }
