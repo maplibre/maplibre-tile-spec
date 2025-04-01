b81361ba9e2e597e0f2735072d38a7dd2efa7169
Francisco Jimenez <jjcfrank@gmail.com>
2025-04-01 21:16:42 +0100

    fix: remove mvt tests

--- rust/mlt/tests/mlt_decoder_test.rs
+++ rust/mlt/tests/mlt_decoder_test.rs
@@ -1,48 +1,29 @@
 mod common;
 
 use mlt::MltResult;
-use mlt::{
-    create_tileset_metadata,
-    mvt::{decode_mvt, ColumnMapping},
-    FeatureTableOptimizations,
-};
-use std::collections::HashMap;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::fs;
 
-fn decode_bing_tiles() -> MltResult<()> {
-    let tile_ids = vec![
-        "4-8-5", "4-9-5", "4-12-6", "4-13-6", "5-16-11", "5-17-11", "5-17-10", "6-32-22",
-        "6-33-22", "6-32-23", "6-32-21", "7-65-42", "7-66-42", "7-66-43", "7-66-44",
-    ];
+#[test]
+fn test_mlt_tiles() -> MltResult<()> {
+    let mlt_path = Path::new("../../test/expected/omt/");
+    let all_files = fs::read_dir(mlt_path)?;
+    
+    // Collect all mlt files
+    let mlt_files: Vec<PathBuf> = all_files
+        .filter_map(Result::ok)
+        .map(|f| f.path())
+        .filter(|f| f.extension().map_or(false, |ext| ext == "mlt"))
+        .collect();
 
-    for tile_id in tile_ids {
-        let bing_mvt_path = Path::new("../../../../test/fixtures/bing");
-        let result = test_tile(tile_id, bing_mvt_path, false);
+    for mlt_file in mlt_files {
+        // Currently, only grab 2_2_2.mlt as it has matching metadata 2_2_2.mlt.meta.pbf
+        if mlt_file.file_name().unwrap() != "2_2_2.mlt" {
+            continue;
+        }
+        println!("Processing: {:?}", mlt_file);
     }
 
     Ok(())
 }
 
-fn test_tile(tile_id: &str, tile_directory: &Path, allow_sorting: bool) -> MltResult<()> {
-    let mvt_file_path = tile_directory.join(format!("{}.mvt", tile_id));
-    let mvt_bytes = std::fs::read(mvt_file_path)?;
-    let mvt_tile = decode_mvt(&mvt_bytes);
-    let column_mappings: Vec<ColumnMapping> = vec![];
-    let tile_metadata = create_tileset_metadata(mvt_tile, true, Some(&column_mappings));
-    let allow_id_regeneration = true;
-
-    let optimizations = FeatureTableOptimizations {
-        allow_sorting,
-        allow_id_regeneration,
-        column_mappings: Some(column_mappings),
-    };
-
-    let optimizations: HashMap<String, FeatureTableOptimizations> = common::OPTIMIZED_MVT_LAYERS
-        .iter()
-        .map(|layer| (layer.to_string(), optimizations.clone()))
-        .collect();
-
-    let include_ids = true;
-
-    Ok(())
-}
