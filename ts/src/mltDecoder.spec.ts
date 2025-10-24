5ceed2f793e670a82d8775731d88bffc63b8f368
Niklas Greindl <niklas.greindl.@rohde-schwarz.com>
2025-10-24 08:21:51 +0200

    change order for better readability

--- ts/src/mltDecoder.spec.ts
+++ ts/src/mltDecoder.spec.ts
@@ -65,7 +65,7 @@ function comparePlainGeometryEncodedTile(
         // Use getFeatures() instead of iterator (like C++ and Java implementations)
         const mltFeatures = featureTable.getFeatures();
 
-        expect(layer.length).toEqual(mltFeatures.length);
+        expect(mltFeatures.length).toEqual(layer.length);
 
         for (let j = 0; j < layer.length; j++) {
             const mvtFeature = layer.feature(j);
