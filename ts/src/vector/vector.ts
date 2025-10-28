47179068e89b5691d8aaf03c930e4090e7febf1a
Benedikt Vogl <benedikt.vogl@rohde-schwarz.com>
2025-10-28 11:15:42 +0100

    remove abstract abstract filter/querry functions from abstract class vector

--- ts/src/vector/vector.ts
+++ ts/src/vector/vector.ts
@@ -82,32 +82,4 @@ export default abstract class Vector<T extends ArrayBufferView = ArrayBufferView
         selectionVector.setLimit(limit);
         return selectionVector;
     }
-
-    protected abstract getValueFromBuffer(index: number): K;
-
-    abstract filter(value: K): SelectionVector;
-
-    abstract filterNotEqual(value: K): SelectionVector;
-
-    abstract match(values: K[]): SelectionVector;
-
-    abstract noneMatch(values: K[]): SelectionVector;
-
-    /* updates the values and limit of the existing SelectionVector in-place */
-    abstract filterSelected(value: K, selectionVector: SelectionVector): void;
-
-    abstract filterNotEqualSelected(value: K, selectionVector: SelectionVector): void;
-
-    /* updates the values and limit of the existing SelectionVector in-place */
-    abstract matchSelected(values: K[], selectionVector: SelectionVector): void;
-
-    abstract noneMatchSelected(values: K[], selectionVector: SelectionVector): void;
-
-    abstract greaterThanOrEqualTo(value: K): SelectionVector;
-
-    abstract smallerThanOrEqualTo(value: K): SelectionVector;
-
-    abstract greaterThanOrEqualToSelected(value: K, selectionVector: SelectionVector): void;
-
-    abstract smallerThanOrEqualToSelected(value: K, selectionVector: SelectionVector): void;
 }
