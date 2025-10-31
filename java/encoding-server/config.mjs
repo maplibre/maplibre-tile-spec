7496cfb2f3d4fc0f3a276e76f9e0d79642ad7129
Bart Louwers <bart@emeel.net>
2025-10-31 20:31:29 +0100

    run formatter

--- java/encoding-server/config.mjs
+++ java/encoding-server/config.mjs
@@ -1,39 +1,43 @@
-import { parseArgs } from 'node:util';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { parseArgs } from "node:util";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const configFile = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const configFile = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "config.json")),
+);
 
 const { values: args } = parseArgs({
   options: {
-    host: { type: 'string', default: '0.0.0.0' },
-    port: { type: 'string', default: '80' },
-    verbose: { type: 'boolean', default: true },
-    keep_files: { type: 'boolean', default: false },
-    noencodingserver: { type: 'boolean', default: false },
-    input: { type: 'string', default: 'mvt' },
-    noids: { type: 'boolean', default: false },
-    advanced: { type: 'boolean', default: false },
-    nomorton: { type: 'boolean', default: false },
-    outlines: { type: 'string', default: '' },
-    timer: { type: 'boolean', default: false },
-    compare: { type: 'boolean', default: false }
+    host: { type: "string", default: "0.0.0.0" },
+    port: { type: "string", default: "80" },
+    verbose: { type: "boolean", default: true },
+    keep_files: { type: "boolean", default: false },
+    noencodingserver: { type: "boolean", default: false },
+    input: { type: "string", default: "mvt" },
+    noids: { type: "boolean", default: false },
+    advanced: { type: "boolean", default: false },
+    nomorton: { type: "boolean", default: false },
+    outlines: { type: "string", default: "" },
+    timer: { type: "boolean", default: false },
+    compare: { type: "boolean", default: false },
   },
-  args: process.argv.slice(2)
+  args: process.argv.slice(2),
 });
 
 const config = {
   ...args,
   ...configFile,
-  port: Number(configFile.port ?? args.port ?? 80)
+  port: Number(configFile.port ?? args.port ?? 80),
 };
 
-
-config.cachePath = path.join(__dirname, 'cache');
-config.cliToolsPath = path.join(__dirname, '../');
-config.encoderPath = path.join(config.cliToolsPath, 'mlt-cli/build/libs/encode.jar');
+config.cachePath = path.join(__dirname, "cache");
+config.cliToolsPath = path.join(__dirname, "../");
+config.encoderPath = path.join(
+  config.cliToolsPath,
+  "mlt-cli/build/libs/encode.jar",
+);
 config.encoderPort = 3001;
 
 export default config;
--- java/encoding-server/server.mjs
+++ java/encoding-server/server.mjs
@@ -1,13 +1,18 @@
-import express from 'express';
+import express from "express";
 
-import config from './config.mjs'
-import { convertStyleRequest, convertSourceRequest, convertTileRequest, runCLISetup } from './convert.mjs'
+import config from "./config.mjs";
+import {
+  convertStyleRequest,
+  convertSourceRequest,
+  convertTileRequest,
+  runCLISetup,
+} from "./convert.mjs";
 
 const app = express();
 
-app.use('/style', convertStyleRequest);
-app.use('/source', convertSourceRequest);
-app.use('/tile', convertTileRequest);
+app.use("/style", convertStyleRequest);
+app.use("/source", convertSourceRequest);
+app.use("/tile", convertTileRequest);
 
 app.listen(config.port, config.host, () => {
   runCLISetup();
