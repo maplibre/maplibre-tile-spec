a46340011d93db9192786fc56377237bfa5dbd73
Niklas Greindl <154528139+Salkin975@users.noreply.github.com>
2025-11-17 14:23:22 +0100

    Add selection vectors (#708)
    adds base methods for re integration of filter methods
    
    - [x] reach test coverage of over 90% on added files
    
    ---------
    
    Co-authored-by: Niklas Greindl <niklas.greindl.@rohde-schwarz.com>

---
+++ ts/src/vector/filter/selectionVector.ts
@@ -0,0 +1,12 @@
+export interface SelectionVector {
+    getIndex(index: number): number;
+    setIndex(index: number, value: number): void;
+    setLimit(limit: number): void;
+    selectionValues(): number[];
+    /* Index of the first element that should not be read or written.
+     * It's not the last index that can be accessed, but rather the index that marks the end of
+     * the valid data in the buffer */
+    get limit();
+    /* Total size of the buffer */
+    get capacity();
+}
