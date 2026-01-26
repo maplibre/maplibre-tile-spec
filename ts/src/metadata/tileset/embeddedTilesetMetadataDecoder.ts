295f0e406e422c379fb99edec56ec46d6ea1c913
Benedikt Vogl <benedikt.vogl@rohde-schwarz.com>
2026-01-26 15:38:07 +0100

    run format

--- ts/src/metadata/tileset/embeddedTilesetMetadataDecoder.ts
+++ ts/src/metadata/tileset/embeddedTilesetMetadataDecoder.ts
@@ -1,11 +1,6 @@
 import type IntWrapper from "../../decoding/intWrapper";
 import { decodeVarintInt32 } from "../../decoding/integerDecodingUtils";
-import {
-    type Column,
-    type FeatureTableSchema,
-    type Field,
-    type TileSetMetadata,
-} from "./tilesetMetadata";
+import { type Column, type FeatureTableSchema, type Field, type TileSetMetadata } from "./tilesetMetadata";
 import { columnTypeHasChildren, columnTypeHasName, decodeColumnType } from "./typeMap";
 
 const textDecoder = new TextDecoder();
