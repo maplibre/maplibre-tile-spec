38bdb608995aa201ddbd99e5a0978ecd424cdf99
Frank Elsinga <frank@elsinga.de>
2026-02-15 20:17:09 +0100

    fix rust tests

--- rust/mlt-nom/tests/synthetics.rs
+++ rust/mlt-nom/tests/synthetics.rs
@@ -6,20 +6,20 @@ use mlt_nom::parse_layers;
 use serde_json::Value;
 use test_each_file::test_each_path;
 
-test_each_path! { for ["mlt", "json"] in "../test/synthetic/0x01" => pair_test }
+test_each_path! { for ["mlt", "json5"] in "../test/synthetic/0x01" => pair_test }
 
-fn pair_test([mlt, json]: [&Path; 2]) {
-    test_one(mlt, json);
+fn pair_test([mlt, json5]: [&Path; 2]) {
+    test_one(mlt, json5);
 }
 
-fn test_one(mlt: &Path, json: &Path) {
+fn test_one(mlt: &Path, json5: &Path) {
     let buffer = fs::read(mlt).unwrap();
     let mut data = parse_layers(&buffer).unwrap();
     for layer in &mut data {
         layer.decode_all().unwrap();
     }
 
-    let expected: FeatureCollection = json5::from_str(&fs::read_to_string(json).unwrap()).unwrap();
+    let expected: FeatureCollection = json5::from_str(&fs::read_to_string(json5).unwrap()).unwrap();
     let actual = FeatureCollection::from_layers(&data).unwrap();
 
     // Normalize very small floats (near 0) to handle precision issues due to serializing to JSON and back
@@ -66,6 +66,6 @@ fn normalize_tiny_floats(value: &Value) -> Value {
 fn test_plain() {
     let path = "../../test/synthetic/0x01/point";
     let mlt = Path::new(path).with_extension("mlt");
-    let json = Path::new(path).with_extension("json");
+    let json = Path::new(path).with_extension("json5");
     test_one(&mlt, &json);
 }
