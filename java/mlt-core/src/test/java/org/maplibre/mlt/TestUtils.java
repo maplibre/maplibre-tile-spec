c5b6b52555f45fcb849c3f54906a1d9b52a67c86
Tim Sylvester <tim@wetdogweather.com>
2026-02-19 13:35:30 -0800

    Fix `compareTilesSequential` calling `.id()` when `f.hasId() == mvtFeature.hasId() == false`.  Remove commented debugging print statements.

--- java/mlt-core/src/test/java/org/maplibre/mlt/TestUtils.java
+++ java/mlt-core/src/test/java/org/maplibre/mlt/TestUtils.java
@@ -3,6 +3,7 @@ package org.maplibre.mlt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Objects;
 import org.maplibre.mlt.converter.mvt.MapboxVectorTile;
 import org.maplibre.mlt.data.Feature;
 import org.maplibre.mlt.data.MapLibreTile;
@@ -26,7 +27,7 @@ public class TestUtils {
       for (org.maplibre.mlt.data.Feature mvtFeature : mvtFeatures) {
         var mltFeature =
             mltFeatures.stream()
-                .filter(f -> f.hasId() == mvtFeature.hasId() && f.id() == mvtFeature.id())
+                .filter(f -> Objects.equals(f.idOrNull(), mvtFeature.idOrNull()))
                 .findFirst()
                 .get();
         assertEquals(mvtFeature.id(), mltFeature.id());
@@ -38,11 +39,6 @@ public class TestUtils {
         var mltProperties = mltFeature.properties();
         var mvtProperties = mvtFeature.properties();
         for (var mvtProperty : mvtProperties.entrySet()) {
-          /*if(mvtProperty.getKey().contains("name:ja:rm")){
-              System.out.println(mvtProperty.getKey() + " " + mvtProperty.getValue() + " " + mltProperties.get(mvtProperty.getKey()) + " " + j + " " + i);
-              continue;
-          }*/
-
           var mltProperty = mltProperties.get(mvtProperty.getKey());
           assertEquals(mvtProperty.getValue(), mltProperty);
         }
@@ -76,9 +72,6 @@ public class TestUtils {
       var mltGeometry = mltFeature.geometry();
       var mvtGeometry = mvtFeature.geometry();
       if (!mvtGeometry.equalsExact(mltGeometry)) {
-        // System.out.println("Failure comparing geometry for feature: " + mvtFeature.id());
-        // System.out.println("    mvtGeometry: " + mvtGeometry);
-        // System.out.println("    mltGeometry: " + mltGeometry);
         numErrors++;
       }
       var mltProperties = mltFeature.properties();
@@ -92,18 +85,8 @@ public class TestUtils {
         }
         var mltProperty = mltProperties.get(mvtPropertyKey);
         if (mltProperty == null) {
-          // System.out.println(mvtFeature.id() + " mlt prop missing for " + mvtProperty.getKey());
-          // System.out.println("  mvtProperties: " + mvtProperties);
-          // System.out.println("  mltProperties: " + mltProperties);
           numErrors++;
         } else if (!mltProperty.equals(mvtProperty.getValue())) {
-          // System.out.println(
-          //     "Failure comparing property "
-          //         + mvtProperty.getKey()
-          //         + " for feature: "
-          //         + mvtFeature.id());
-          // System.out.println("    mvtProperty: " + mvtProperty.getValue());
-          // System.out.println("    mltProperty: " + mltProperty);
           numErrors++;
         } else {
           assertEquals(
@@ -127,7 +110,7 @@ public class TestUtils {
         "mltLayers.size()=" + mltLayers.size() + " mvtLayers.size()=" + mvtLayers.size());
     for (var i = 0; i < mvtLayers.size(); i++) {
       var mvtLayer = mvtLayers.get(i);
-      var mvtFeatures = mvtLayers.get(i).features();
+      var mvtFeatures = mvtLayer.features();
       var mltLayer = mltLayers.get(i);
       var mltFeatures = mltLayer.features();
       numErrors += compareFeatures(mltFeatures, mvtFeatures, isFeatureTableSorted);
