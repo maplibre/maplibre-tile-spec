5c8a12e6434fd572cc35930177d26f640ca502ad
Tim Sylvester <tim@wetdogweather.com>
2026-02-19 13:27:11 -0800

    Remove dead code

--- java/mlt-core/src/test/java/org/maplibre/mlt/TestUtils.java
+++ java/mlt-core/src/test/java/org/maplibre/mlt/TestUtils.java
@@ -2,99 +2,16 @@ package org.maplibre.mlt;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.maplibre.mlt.converter.mvt.MapboxVectorTile;
 import org.maplibre.mlt.data.Feature;
 import org.maplibre.mlt.data.MapLibreTile;
-import org.maplibre.mlt.vector.FeatureTable;
 
 public class TestUtils {
   public enum Optimization {
     NONE,
     SORTED,
     IDS_REASSIGNED
-  }
-
-  public static void compareTilesVectorized(
-      FeatureTable[] featureTables,
-      MapboxVectorTile mvTile,
-      Optimization optimization,
-      List<String> reassignableLayers) {
-    var mvtLayers = mvTile.layers();
-    for (var i = 0; i < mvtLayers.size(); i++) {
-      var featureTable = featureTables[i];
-      var mvtLayer = mvtLayers.get(i);
-      var mvtFeatures = mvtLayer.features();
-      var featureIterator = featureTable.iterator();
-      for (var j = 0; j < mvtFeatures.size(); j++) {
-        var mltFeature = featureIterator.next();
-        Feature mvtFeature =
-            optimization == Optimization.SORTED
-                ? mvtFeatures.stream()
-                    .filter(f -> f.hasId() == mltFeature.hasId() && f.id() == mltFeature.id())
-                    .findFirst()
-                    .get()
-                : mvtFeatures.get(j);
-
-        /* Test for a sequential order if the  id was reassigned  */
-        var mvtId =
-            optimization == Optimization.IDS_REASSIGNED
-                    && reassignableLayers.stream().anyMatch(l -> l.equals(featureTable.getName()))
-                ? (long) j
-                : mvtFeature.id();
-        try {
-          assertEquals(mvtId, mltFeature.id());
-        } catch (Error e) {
-          System.out.println(e);
-        }
-
-        var mvtGeometry = mvtFeature.geometry();
-        var mltGeometry = mltFeature.geometry();
-        assertEquals(mvtGeometry, mltGeometry);
-
-        var mltProperties = mltFeature.properties();
-        for (var property : mltProperties.entrySet()) {
-          var mltPropertyKey = property.getKey();
-          var mltPropertyValue = property.getValue();
-          if (mltPropertyValue instanceof Map<?, ?>) {
-            /* Handle shared dictionary case -> currently only String is supported
-             * as nested property in the converter, so only handle this case */
-            var mvtProperties = mvtFeature.properties();
-
-            @SuppressWarnings("unchecked")
-            var nestedStringValues = (Map<String, String>) mltPropertyValue;
-            var mvtStringProperties =
-                mvtProperties.entrySet().stream()
-                    .filter(
-                        p -> p.getKey().contains(mltPropertyKey) && p.getValue() instanceof String)
-                    .toList();
-            // TODO: verify why mlt seems to have a property more than mvt on the
-            // name:* column in some tiles
-            for (var mvtProperty : mvtStringProperties) {
-              var mvtPropertyKey = mvtProperty.getKey();
-              var mvtPropertyValue = mvtProperty.getValue();
-              var mltValue = nestedStringValues.get(mvtPropertyKey);
-
-              if (mvtPropertyKey.equals("name:ja:rm") || mvtPropertyKey.equals("name:ja:kana")) {
-                // TODO: fix -> currently the converter can't handle a triple nested property name
-                System.out.println(
-                    "Skip verification for the name:ja:rm property name since it is currently"
-                        + "not supported in the converter.");
-                continue;
-              }
-
-              // TODO: fix
-              assertEquals(mvtPropertyValue, mltValue);
-            }
-          } else {
-            // TODO: fix int and long type problems
-            assertEquals(mvtFeature.properties().get(mltPropertyKey), mltPropertyValue);
-          }
-        }
-      }
-    }
   }
 
   public static void compareTilesSequential(MapLibreTile mlTile, MapboxVectorTile mvTile) {
@@ -195,28 +112,6 @@ public class TestUtils {
               "mvtProperty.getValue()=" + mvtProperty.getValue() + " mltProperty=" + mltProperty);
         }
       }
-    }
-    return numErrors;
-  }
-
-  public static int compareTilesVectorized(
-      FeatureTable[] featureTables, MapboxVectorTile mvTile, boolean isFeatureTableSorted) {
-    int numErrors = 0;
-    var mvtLayers = mvTile.layers();
-    assertEquals(
-        featureTables.length,
-        mvtLayers.size(),
-        "featureTables.length=" + featureTables.length + " mvtLayers.size()=" + mvtLayers.size());
-    for (var i = 0; i < mvtLayers.size(); i++) {
-      var mvtLayer = mvtLayers.get(i);
-      var mvtFeatures = mvtLayers.get(i).features();
-      var featureTable = featureTables[i];
-      var featureIterator = featureTable.iterator();
-      var mltFeatures = new ArrayList<Feature>();
-      while (featureIterator.hasNext()) {
-        mltFeatures.add(featureIterator.next());
-      }
-      numErrors += compareFeatures(mltFeatures, mvtFeatures, isFeatureTableSorted);
     }
     return numErrors;
   }
