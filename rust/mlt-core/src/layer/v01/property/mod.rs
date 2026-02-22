4c063a9a35caca794e0515aac0f2869f989d0594
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2026-02-22 12:28:36 -0500

    inline

--- rust/mlt-core/src/layer/v01/property/mod.rs
+++ rust/mlt-core/src/layer/v01/property/mod.rs
@@ -449,12 +449,11 @@ impl FromDecoded<'_> for OwnedEncodedProperty {
         use {OwnedEncodedPropValue as EncVal, PropValue as Val};
         let optional = if config.optional == PresenceSteamStrategy::Present {
             let present_vec: Vec<bool> = decoded.values.as_presence_stream()?;
-            let num_values = u32::try_from(present_vec.len())?;
             let data = encode_byte_rle(&encode_bools_to_bytes(&present_vec));
             Some(OwnedStream {
                 meta: StreamMeta {
                     physical_type: PhysicalStreamType::Present,
-                    num_values,
+                    num_values: u32::try_from(present_vec.len())?,
                     logical_codec: LogicalCodec::None,
                     physical_codec: PhysicalCodec::None,
                 },
