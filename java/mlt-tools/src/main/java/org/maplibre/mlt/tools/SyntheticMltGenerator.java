maplibre/maplibre-tile-spec

commit 7385200c4bfccd982365a9d06d9478b9b056977a

test(synthetic): more floating point tests (#988)
Floats are a bit weird, these are a few of the values I learned exists
-> we likely should cover them too

java/mlt-tools/src/main/java/org/maplibre/mlt/tools/SyntheticMltGenerator.java
@@ -188,20 +188,27 @@ public class SyntheticMltGenerator {
         cfg());
     write("prop_f32", feat(p0, prop("val", (float) 3.14f)), cfg());
     write("prop_f32_neg_inf", feat(p0, prop("val", Float.NEGATIVE_INFINITY)), cfg());
-    write("prop_f32_min", feat(p0, prop("val", Float.MIN_VALUE)), cfg());
+    write("prop_f32_min_exp", feat(p0, prop("val", Float.MIN_EXPONENT)), cfg());
+    write("prop_f32_min_norm", feat(p0, prop("val", Float.MIN_NORMAL)), cfg());
+    // FIXME: Produces the same output as prop_f32_min_norm
+    // write("prop_f32_min_val", feat(p0, prop("val", Float.MIN_VALUE)), cfg());
     // FIXME: Produces the same output as prop_f32_min
     // write("prop_f32_neg_zero", feat(p0, prop("val", (float) -0.0f)), cfg());
     write("prop_f32_zero", feat(p0, prop("val", (float) 0.0f)), cfg());
-    write("prop_f32_max", feat(p0, prop("val", Float.MAX_VALUE)), cfg());
+    write("prop_f32_max_val", feat(p0, prop("val", Float.MAX_VALUE)), cfg());
+    write("prop_f32_max_exp", feat(p0, prop("val", Float.MAX_EXPONENT)), cfg());
     write("prop_f32_pos_inf", feat(p0, prop("val", Float.POSITIVE_INFINITY)), cfg());
     write("prop_f32_nan", feat(p0, prop("val", Float.NaN)), cfg());
     write("prop_f64", feat(p0, prop("val", (double) 3.141592653589793)), cfg());
     write("prop_f64_neg_inf", feat(p0, prop("val", Double.NEGATIVE_INFINITY)), cfg());
-    write("prop_f64_min", feat(p0, prop("val", Double.MIN_VALUE)), cfg());
+    write("prop_f64_min_exp", feat(p0, prop("val", Double.MIN_EXPONENT)), cfg());
+    write("prop_f64_min_norm", feat(p0, prop("val", Double.MIN_NORMAL)), cfg());
+    write("prop_f64_min_val", feat(p0, prop("val", Double.MIN_VALUE)), cfg());
     write("prop_f64_neg_zero", feat(p0, prop("val", (double) -0.0)), cfg());
     // FIXME: Produces the same output as prop_f64_min
     // write("prop_f64_zero", feat(p0, prop("val", (double) 0.0)), cfg());
-    write("prop_f64_max", feat(p0, prop("val", Double.MAX_VALUE)), cfg());
+    write("prop_f64_max_val", feat(p0, prop("val", Double.MAX_VALUE)), cfg());
+    write("prop_f64_max_exp", feat(p0, prop("val", Double.MAX_EXPONENT)), cfg());
     // FIXME: Fails in Java as it Produces the same output as prop_f64_max
     // write("prop_f64_pos_inf", feat(p0, prop("val", Double.POSITIVE_INFINITY)), cfg());
     write("prop_f64_nan", feat(p0, prop("val", Double.NaN)), cfg());

commit e17fd98b05f55c2e4d8b7fe064619ba47d9e6dd8

fix(ci): comment-out dup test (#992)
This was accidentally broken in
https://github.com/maplibre/maplibre-tile-spec/pull/988

java/mlt-tools/src/main/java/org/maplibre/mlt/tools/SyntheticMltGenerator.java
@@ -203,7 +203,8 @@ public class SyntheticMltGenerator {
     write("prop_f64_neg_inf", feat(p0, prop("val", Double.NEGATIVE_INFINITY)), cfg());
     write("prop_f64_min_exp", feat(p0, prop("val", Double.MIN_EXPONENT)), cfg());
     write("prop_f64_min_norm", feat(p0, prop("val", Double.MIN_NORMAL)), cfg());
-    write("prop_f64_min_val", feat(p0, prop("val", Double.MIN_VALUE)), cfg());
+    // FIXME: Produces the same output as prop_f64_min_norm
+    // write("prop_f64_min_val", feat(p0, prop("val", Double.MIN_VALUE)), cfg());
     write("prop_f64_neg_zero", feat(p0, prop("val", (double) -0.0)), cfg());
     // FIXME: Produces the same output as prop_f64_min
     // write("prop_f64_zero", feat(p0, prop("val", (double) 0.0)), cfg());