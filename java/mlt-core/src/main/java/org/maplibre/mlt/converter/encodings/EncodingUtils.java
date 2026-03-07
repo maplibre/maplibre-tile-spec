a6c804896f24ad23e3e53c56bd466d68f6d034fe
Tim Sylvester <tim@wetdogweather.com>
2026-03-06 16:42:34 -0800

    Generate varints directly into `ByteBuffer` instead of tracking position manually

--- java/mlt-core/src/main/java/org/maplibre/mlt/converter/encodings/EncodingUtils.java
+++ java/mlt-core/src/main/java/org/maplibre/mlt/converter/encodings/EncodingUtils.java
@@ -21,7 +21,6 @@ import me.lemire.integercompression.VariableByte;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.maplibre.mlt.converter.CollectionUtils;
-import org.maplibre.mlt.decoder.DecodingUtils;
 
 public class EncodingUtils {
 
@@ -81,12 +80,11 @@ public class EncodingUtils {
       encodedValues = encodeZigZag(encodedValues);
     }
 
-    var varintBuffer = new byte[values.length * MAX_VARLONG_SIZE];
-    var i = 0;
+    var varintBuffer = ByteBuffer.wrap(new byte[values.length * MAX_VARLONG_SIZE]);
     for (var value : encodedValues) {
-      i = putVarInt(value, varintBuffer, i);
+      putVarInt(value, varintBuffer);
     }
-    return ByteBuffer.wrap(varintBuffer, 0, i);
+    return varintBuffer.flip();
   }
 
   public static ByteBuffer encodeVarints(long[] values, boolean zigZagEncode, boolean deltaEncode)
@@ -100,12 +98,11 @@ public class EncodingUtils {
       encodedValues = encodeZigZag(encodedValues);
     }
 
-    var varintBuffer = new byte[values.length * MAX_VARLONG_SIZE];
-    var i = 0;
+    var varintBuffer = ByteBuffer.wrap(new byte[values.length * MAX_VARLONG_SIZE]);
     for (var value : encodedValues) {
-      i = putVarInt(value, varintBuffer, i);
+      putVarInt(value, varintBuffer);
     }
-    return ByteBuffer.wrap(varintBuffer, 0, i);
+    return varintBuffer.flip();
   }
 
   public static ByteBuffer encodeVarints(
@@ -122,8 +119,7 @@ public class EncodingUtils {
     if (zigZagEncode) {
       value = encodeZigZag(value);
     }
-    final var varintBuffer = new byte[MAX_VARLONG_SIZE];
-    return ByteBuffer.wrap(varintBuffer, 0, putVarInt(value, varintBuffer, 0));
+    return putVarInt(value, ByteBuffer.wrap(new byte[MAX_VARLONG_SIZE])).flip();
   }
 
   // Source:
@@ -134,56 +130,32 @@ public class EncodingUtils {
    *
    * @param v the int value to write to sink
    * @param sink the sink buffer to write to
-   * @param offset the offset within sink to begin writing
-   * @return the updated offset after writing the varint
    */
-  static int putVarInt(int v, byte[] sink, @SuppressWarnings("SameParameterValue") int offset)
-      throws IOException {
-    final var checkValue = v;
-    var sinkRemaining = Math.min(sink.length - offset, MAX_VARINT_SIZE);
-    var sinkUsed = 0;
+  static ByteBuffer putVarInt(int v, ByteBuffer sink) throws IOException {
     do {
       // Encode next 7 bits + terminator bit
       final int bits = v & 0x7F;
       v >>>= 7;
-      final byte b = (byte) (bits + ((v != 0) ? 0x80 : 0));
-      if (sinkRemaining - sinkUsed < 1) {
-        throw new IOException("Varint overflow");
-      }
-      sink[offset + sinkUsed++] = b;
+      sink.put((byte) (bits + ((v != 0) ? 0x80 : 0)));
     } while (v != 0);
-
-    // ensure that the result decodes back into the input
-    assert DecodingUtils.decodeVarints(sink, new IntWrapper(offset), 1)[0] == checkValue
-        : "Varint Overflow";
-
-    return offset + sinkUsed;
+    return sink;
   }
 
-  static int putVarInt(long v, byte[] sink, int offset) throws IOException {
-    final var checkValue = v;
-    var sinkRemaining = Math.min(sink.length - offset, MAX_VARLONG_SIZE);
-    var sinkUsed = 0;
+  static ByteBuffer putVarInt(long v, ByteBuffer sink) throws IOException {
     do {
       // Encode next 7 bits + terminator bit
       final long bits = v & 0x7F;
       v >>>= 7;
-      final byte b = (byte) (bits + ((v != 0) ? 0x80 : 0));
-      if (sinkRemaining - sinkUsed < 1) {
-        throw new IOException("Varint overflow");
-      }
-      sink[offset + sinkUsed++] = b;
+      sink.put((byte) (bits + ((v != 0) ? 0x80 : 0)));
     } while (v != 0);
-
-    assert DecodingUtils.decodeLongVarint(sink, new IntWrapper(offset)) == checkValue
-        : "Varint Overflow";
-    return offset + sinkUsed;
+    return sink;
   }
 
   @SuppressWarnings("UnusedReturnValue")
   public static DataOutputStream putVarInt(DataOutputStream stream, int v) throws IOException {
-    final var buffer = new byte[MAX_VARINT_SIZE];
-    stream.write(buffer, 0, putVarInt(v, buffer, 0));
+    final var buffer = ByteBuffer.wrap(new byte[MAX_VARINT_SIZE]);
+    putVarInt(v, buffer);
+    stream.write(buffer.array(), 0, buffer.position());
     return stream;
   }
 
