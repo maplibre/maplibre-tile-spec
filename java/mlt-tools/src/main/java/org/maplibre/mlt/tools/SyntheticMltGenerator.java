521fc3c5aa7a81b56f60cf8d6a0e2843dedf25da
Frank Elsinga <frank@elsinga.de>
2026-03-08 15:59:31 +0100

    fix fmt

--- java/mlt-tools/src/main/java/org/maplibre/mlt/tools/SyntheticMltGenerator.java
+++ java/mlt-tools/src/main/java/org/maplibre/mlt/tools/SyntheticMltGenerator.java
@@ -165,10 +165,10 @@ public class SyntheticMltGenerator {
     write("id", idFeat(100), cfg().ids());
     write("id_min", idFeat(0), cfg().ids());
     // FIXME: serialises as i32
-    //write("id_max", idFeat(0xFFFF), cfg().ids());
+    // write("id_max", idFeat(0xFFFF), cfg().ids());
     write("id64", idFeat(9_234_567_890L), cfg().ids());
     // FIXME: serialises as i64
-    //write("id64_max", idFeat(0xFFFFFFFFL), cfg().ids());
+    // write("id64_max", idFeat(0xFFFFFFFFL), cfg().ids());
 
     var ids32 = array(idFeat(103), idFeat(103), idFeat(103), idFeat(103));
     write(layer("ids", ids32), cfg().ids());
@@ -197,9 +197,9 @@ public class SyntheticMltGenerator {
 
     // java doe not generate this as an ID64 if none of them are above the U32::Max threshold
     // FIXME: serialises as i64
-    //var ids64MinMax = array(idFeat(0L), idFeat(0xFFFFFFFFL), idFeat(0L), idFeat(0xFFFFFFFFL));
-    //write(layer("ids64_minmax", ids64MinMax), cfg().ids());
-    //write(layer("ids64_minmax_delta", ids64MinMax), cfg(DELTA).ids());
+    // var ids64MinMax = array(idFeat(0L), idFeat(0xFFFFFFFFL), idFeat(0L), idFeat(0xFFFFFFFFL));
+    // write(layer("ids64_minmax", ids64MinMax), cfg().ids());
+    // write(layer("ids64_minmax_delta", ids64MinMax), cfg(DELTA).ids());
   }
 
   @SuppressWarnings("cast")
