e334a6275d12a8be8a75d7eddb26fbcacf595251
Tim Sylvester <tim@wetdogweather.com>
2026-03-27 08:30:45 -0700

    Fix Javadoc errors

--- java/mlt-core/src/main/java/org/maplibre/mlt/data/Property.java
+++ java/mlt-core/src/main/java/org/maplibre/mlt/data/Property.java
@@ -1,23 +1,42 @@
 package org.maplibre.mlt.data;
 
+import java.util.Objects;
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 import org.maplibre.mlt.metadata.tileset.MltMetadata;
 
+/** Represents a property of a feature */
 public class Property {
-  @Getter private final MltMetadata.FieldType type;
+  @Getter @NotNull private final MltMetadata.FieldType type;
   @Getter private final String name;
   private final Object value;
 
+  /**
+   * Create directly from a scalar type, for convenience
+   *
+   * @param type The scalar type of the property
+   * @param name The name of the property
+   * @param value The value of the property, which should be compatible with the type. No validation
+   *     is performed.
+   */
   public Property(MltMetadata.ScalarType type, String name, Object value) {
     this(MltMetadata.scalarFieldBuilder(type).build(), name, value);
   }
 
-  public Property(MltMetadata.FieldType type, String name, Object value) {
-    this.type = type;
+  /**
+   * Create directly from a complex type
+   *
+   * @param type The type of the property
+   * @param name The name of the property
+   * @param value The value of the property, which should be compatible with the type. No validation
+   *     is performed.
+   */
+  public Property(@NotNull MltMetadata.FieldType type, String name, Object value) {
+    this.type = Objects.requireNonNull(type);
     this.name = name;
     this.value = value;
 
-    if (!isNested() && value != null) {
+    if (!isNested(type) && value != null) {
       if (type.scalarType == null
           || type.scalarType.physicalType.ordinal() < MltMetadata.ScalarType.BOOLEAN.ordinal()
           || type.scalarType.physicalType.ordinal() > MltMetadata.ScalarType.STRING.ordinal()) {
@@ -27,12 +46,23 @@ public class Property {
     }
   }
 
+  /**
+   * @param featureIndex The index of the feature for which to get the value
+   * @return The value of the property.
+   */
   public Object getValue(int featureIndex) {
     return value;
   }
 
-  public boolean isNested() {
+  private static boolean isNested(MltMetadata.FieldType type) {
     return (type.complexType != null
         && type.complexType.physicalType == MltMetadata.ComplexType.MAP);
+  }
+
+  /**
+   * @return True if the property is nested
+   */
+  public boolean isNested() {
+    return isNested(type);
   }
 }
