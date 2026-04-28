529c13d628587e29d214512c7f964b42063a2423
Thomas Fuhrmann <thomas.fuhrmann@geosphere.at>
2026-04-28 14:44:05 +0200

    feat(rust): parallelize pmtiles writer

--- rust/mlt/src/convert/tileset.rs
+++ rust/mlt/src/convert/tileset.rs
@@ -1,17 +1,18 @@
-use std::{fs::File, path::Path};
+use std::{path::Path, sync::Arc, thread};
 
-use anyhow::{Ok, Result as AnyResult, anyhow, bail};
+use anyhow::{Result as AnyResult, anyhow, bail};
+use bytes::Bytes;
 use futures::StreamExt;
 use martin_tile_utils::{Encoding, Format};
-use mbtiles::{MbtType, Mbtiles, MbtilesTranscoder};
+use mbtiles::{MbtType, Mbtiles, MbtilesTranscoder, Metadata};
 use mlt_core::encoder::EncoderConfig;
-use pmtiles::{PmTilesWriter, TileType};
+use pmtiles::{PmTilesWriter, TileCoord, TileType};
 
 use crate::convert::{ConvertArgs, SortMode, TileFormat};
 
 use super::{MbtFormat, encode_one};
 
-async fn get_encoding_type(input: &Path) -> AnyResult<(Encoding, MbtType)> {
+async fn get_metadata(input: &Path) -> AnyResult<(Encoding, MbtType, Metadata)> {
     let src = Mbtiles::new(input)?;
     let mut src_conn = src.open_readonly().await?;
 
@@ -30,7 +31,7 @@ async fn get_encoding_type(input: &Path) -> AnyResult<(Encoding, MbtType)> {
     }
 
     let src_type = src.detect_type(&mut src_conn).await?;
-    Ok((tile_info.encoding, src_type))
+    Ok((tile_info.encoding, src_type, meta))
 }
 
 pub async fn convert_tiles(args: &ConvertArgs) -> AnyResult<()> {
@@ -63,7 +64,7 @@ async fn convert_mbtiles_to_mbtiles(
     mbtiles_format: Option<MbtFormat>,
     cfg: EncoderConfig,
 ) -> AnyResult<()> {
-    let (encoding, src_type) = get_encoding_type(input).await?;
+    let (encoding, src_type, _) = get_metadata(input).await?;
     let mbt_type = mbtiles_format.map_or(src_type, Into::into);
 
     eprintln!("{} → {} ({mbt_type}):", input.display(), output.display());
@@ -102,23 +103,70 @@ async fn convert_mbtiles_to_pmtiles(
     output: &Path,
     cfg: EncoderConfig,
 ) -> AnyResult<()> {
-    let (encoding, _) = get_encoding_type(input).await?;
+    let (raw_tx, raw_rx) = tokio::sync::mpsc::channel::<(TileCoord, Vec<u8>)>(500);
+    let (final_tx, mut final_rx) = tokio::sync::mpsc::channel::<(TileCoord, Bytes)>(500);
+
+    // Read from .mbtiles
     let mbt = Mbtiles::new(input)?;
-    let mut conn: sqlx::SqliteConnection = mbt.open_readonly().await?;
-    let mut stream = mbt.stream_tiles(&mut conn);
-
-    let file = File::create(output)?;
-    let mut writer = PmTilesWriter::new(TileType::Mlt).create(file).unwrap();
-    while let Some(tile) = stream.next().await {
-        let (coord, data) = tile?;
-
-        if let Some(data) = data {
-            let coord = pmtiles::TileCoord::new(coord.z, coord.x, coord.y).unwrap();
-            let enc_data = encode_one(data, encoding, cfg)?;
-            writer.add_tile(coord, &enc_data[..])?;
+    let mut conn = mbt.open_readonly().await?;
+    tokio::spawn(async move {
+        let mut stream = mbt.stream_tiles(&mut conn);
+        while let Some(tile_result) = stream.next().await {
+            match tile_result {
+                Ok((coord, Some(data))) => {
+                    if let Ok(c) = TileCoord::new(coord.z, coord.x, coord.y) {
+                        if raw_tx.send((c, data)).await.is_err() {
+                            break;
+                        }
+                    }
+                }
+                Ok((_, None)) => continue,
+                Err(e) => {
+                    eprintln!("Database stream error: {}", e);
+                    break;
+                }
+            }
         }
+    });
+
+    // Encode to mlt
+    let (encoding, _, mut metadata) = get_metadata(input).await?;
+    let raw_rx = Arc::new(tokio::sync::Mutex::new(raw_rx));
+    let num_cpus = thread::available_parallelism()
+        .map(|n| n.get())
+        .unwrap_or(1);
+    for _ in 0..num_cpus {
+        let input = raw_rx.clone();
+        let output = final_tx.clone();
+
+        tokio::spawn(async move {
+            while let Some((coord, data)) = {
+                let mut lock = input.lock().await;
+                lock.recv().await
+            } {
+                if let Ok(enc_data) = encode_one(data, encoding, cfg) {
+                    output.send((coord, enc_data)).await.ok();
+                }
+            }
+        });
     }
-    writer.finalize()?;
+    drop(final_tx);
+
+    // Write to .pmtiles
+    metadata.tilejson.other.insert(
+        "format".into(),
+        serde_json::Value::String(Format::Mlt.metadata_format_value().into()),
+    );
+    let file = std::fs::File::create(output)?;
+    let metadata_str = serde_json::to_string(&metadata.tilejson)?;
+    let mut stream_writer = PmTilesWriter::new(TileType::Mlt)
+        .metadata(&metadata_str)
+        .create(file)?;
+    while let Some((coord, data)) = final_rx.recv().await {
+        stream_writer.add_tile(coord, &data)?;
+    }
+
+    stream_writer.finalize()?;
 
     Ok(())
 }
