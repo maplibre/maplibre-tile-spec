057fdb354f5909bbd7aaef752e14d7f59e17b0bf
Frank Elsinga <frank@elsinga.de>
2026-09-05 01:42:07 +0200

    chore(ci): consolidate and properly merge Coverage (#1640)
    Currently, we run CI twice. Once under codecov, once without.
    This PR adds an merging of coverage step and reports this to github
    instead...
    
    Means no good line coverage instrumentation, but since we only
    intermittently build ("only what changed"), the line coverage is not
    reliable anyhow.
    Switching to GH means we can add a ruleset for merging on this 🎉

--- ts/eslint.config.mjs
+++ ts/eslint.config.mjs
@@ -49,6 +49,6 @@ export default tseslint.config(
         },
     },
     {
-        ignores: ["dist", "node_modules", "coverage", "*.config.mjs", "*.config.js", "**/*.js"],
+        ignores: ["dist", "node_modules", "coverage", "*.config.mjs", "*.config.js", "*.config.ts", "**/*.js"],
     },
 );
