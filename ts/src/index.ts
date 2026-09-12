0426cb6d67d2f4929f4b7a56454b51659cfc78fc
Hiroaki Yutani <yutani.ini@gmail.com>
2026-09-12 18:27:20 +0900

    export MapEncodingOptions

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -8,6 +8,7 @@ export { GEOMETRY_TYPE } from "./vector/geometry/geometryType";
 export type { TileSetMetadata } from "./metadata/tileset/tilesetMetadata";
 export type { Geometry } from "./vector/geometry/geometryVector";
 export type { Feature } from "./vector/featureTable";
+export type { MapEncodingOptions } from "./encoding/mapPropertyEncoder";
 export type {
     EncodeOptions,
     Feature as FeatureInput,
